2adcb27a08b720f7e6c8dd02fe3e596a02d86e55
Vincent_Liussyaputra <liusvinv@gmail.com>
2024-03-03 09:44:40 +0700

    Fix metadata

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import PageTitle from '../components/layout/pageTitle/PageTitle'
-import { FaX, FaXTwitter } from 'react-icons/fa6'
+import { FaDiscord, FaThreads, FaTiktok, FaTwitch, FaX, FaXTwitter, FaYoutube } from 'react-icons/fa6'
 import './contacts.scss'
 type Props = {}
 
@@ -10,25 +10,25 @@ export default function Contacts({}: Props) {
 			<PageTitle subtitle='List of contacts' title='Contacts'/>
 			<div className="contact-container">
 				<div className="listcont left-contact">
-						<a href='#' className="contact">
+						<a href='https://discord.com/invite/hqWVaBQyn7' className="contact">
 								<span className='contact-content'>
 									<span className='media-type'>DISCORD</span>							
-									<span className='handles'>@handle_sub</span>		
-									<FaXTwitter className='contact-icon'/>				
+									<span className='handles'>@astramaeve</span>		
+									<FaDiscord className='contact-icon'/>				
 								</span>		
 						</a>
-						<a href='#' className="contact offset">
+						<a href='https://twitter.com/astra_maeve' className="contact offset">
 								<span className='contact-content'>
-									<span className='media-type'>DISCORD</span>							
-									<span className='handles'>@handle_sub</span>		
+									<span className='media-type'>Twitter</span>							
+									<span className='handles'>@astra_maeve</span>		
 									<FaXTwitter className='contact-icon'/>				
 								</span>		
 						</a>		
-						<a href='#' className="contact">
+						<a href='https://www.twitch.tv/astramaeve' className="contact">
 								<span className='contact-content'>
-									<span className='media-type'>DISCORD</span>							
-									<span className='handles'>@handle_sub</span>		
-									<FaXTwitter className='contact-icon'/>				
+									<span className='media-type'>Twitch</span>							
+									<span className='handles'>astramaeve</span>		
+									<FaTwitch className='contact-icon'/>				
 								</span>		
 						</a>
 				</div>
@@ -38,29 +38,29 @@ export default function Contacts({}: Props) {
 						<img src="/arts/contact-banner.png" alt="" className='maeve-banner' />
 					</div>
 					<img src="/arts/maeve-main-logo.png" alt="" className='maeve-logo' />
-					<a  href="mailto:astralias.maeve@gmail.com" className='email'><strong>email</strong><br />astralias.maeve@gmail.com</a>
+					<a  href="mailto:astralias.maeve@gmail.com" className='email'><strong>email</strong><br /><span>astralias.maeve@gmail.com</span></a>
 				</div>
 
 				<div className="listcont right-contact">
 						<a href='#' className="contact">
 								<span className='contact-content'>
-									<span className='media-type'>DISCORD</span>							
-									<span className='handles'>@handle_sub</span>		
-									<FaXTwitter className='contact-icon'/>				
+									<span className='media-type'>THREADS</span>							
+									<span className='handles'>@astramaeve</span>		
+									<FaThreads className='contact-icon'/>				
 								</span>		
 						</a>
-						<a href='#' className="contact offset">
+						<a href='https://www.youtube.com/@astramaeve' className="contact offset">
 								<span className='contact-content'>
-									<span className='media-type'>DISCORD</span>							
-									<span className='handles'>@handle_sub</span>		
-									<FaXTwitter className='contact-icon'/>				
+									<span className='media-type'>YOUTUBE</span>							
+									<span className='handles'>Astra Maeve </span>		
+									<FaYoutube className='contact-icon'/>				
 								</span>		
 						</a>		
-						<a href='#' className="contact">
+						<a href='https://www.tiktok.com/@astramaeve' className="contact">
 								<span className='contact-content'>
-									<span className='media-type'>DISCORD</span>							
-									<span className='handles'>@handle_sub</span>		
-									<FaXTwitter className='contact-icon'/>				
+									<span className='media-type'>TIKTOK</span>							
+									<span className='handles'>@astramaeve</span>		
+									<FaTiktok className='contact-icon'/>				
 								</span>		
 						</a>
 				</div>
